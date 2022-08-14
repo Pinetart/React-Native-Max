356ecd9c3437fe7bd6323e1eb01f495a1faaeb0c
Zane Birkett <axelbirkett@gmail.com>
2022-08-14 14:36:51 -0400

    Finishing the Meal Detail Screen

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,6 +1,8 @@
 import { Image, StyleSheet, Text, View } from "react-native";
 import { MEALS } from "../data/dummy-data";
 import MealDetails from "../components/MealDetails";
+import Subtitle from "../components/MealDetail/Subtitle";
+import List from "../components/MealDetail/List";
 
 const MealDetailsScreen = ({ route }) => {
   const mealId = route.params.mealId;
@@ -9,25 +11,38 @@ const MealDetailsScreen = ({ route }) => {
   return (
     <View>
       <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
-      <Text>{selectedMeal.title}</Text>
+      <Text style={styles.title}>{selectedMeal.title}</Text>
       <MealDetails
         duration={selectedMeal.duration}
         complexity={selectedMeal.complexity}
         affordability={selectedMeal.affordability}
+        textStyle={styles.detailText}
       />
-      <Text>Ingredients</Text>
-      {selectedMeal.ingredients.map((ingredient) => (
-        <Text key={ingredient}>{ingredient}</Text>
-      ))}
-      <Text>Steps</Text>
-      {selectedMeal.steps.map((step) => (
-        <Text key={step}>{step}</Text>
-      ))}
+      <View style={styles.listOuterContainer}>
+        <View style={styles.listContainer}>
+          <Subtitle>Ingredients</Subtitle>
+          <List data={selectedMeal.ingredients} />
+          <Subtitle>Steps</Subtitle>
+          <List data={selectedMeal.steps} />
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  image: { width: "100%", height: 200 },
+  image: { width: "100%", height: 350 },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    margin: 8,
+    textAlign: "center",
+    color: "white",
+  },
+  detailText: { color: "white" },
+  listContainer: { width: "80%" },
+  listOuterContainer: {
+    alignItems: "center",
+  },
 });
 export default MealDetailsScreen;
